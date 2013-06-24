c22d5a1a747e7b93bed1a63d1aa75463655a9e2f
Ben Eills <ben@beneills.com>
2013-06-24 19:47:23 +0100

    Delete old emacs files

--- emacs.d_old/.emacs.d
+++
@@ -1,1 +0,0 @@
-/home/ziarkaen/.emacs.d
--- emacs.d_old/modeline.el
+++
@@ -1,9 +0,0 @@
-(column-number-mode t)                   ;; show column numbers
-
-;; TODO
-;; (if (require 'sml-modeline nil 'noerror)    ;; use sml-modeline if available
-;;   (progn 
-;;     (sml-modeline-mode 1)                   ;; show buffer pos in the mode line
-;;     (scroll-bar-mode -1))                   ;; turn off the scrollbar
-;;   (scroll-bar-mode 1)                       ;; otherwise, show a scrollbar...
-;;   (set-scroll-bar-mode 'right))             ;; ... on the right
