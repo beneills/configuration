c4c426b84440a4041d0135611be03ffde5132db7
Ben Eills <ben@beneills.com>
2013-07-01 19:17:07 +0100

    + plan keybinding

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,6 +8,7 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 (global-set-key "\C-cs" 'shell)
+(global-set-key "\C-cp" 'plan)
 
 
 ;;;;
