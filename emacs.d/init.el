048f46c62936c981c567657772de42ef205011f7
Ben Eills <ben@beneills.com>
2013-07-01 18:38:00 +0100

    dotfile visibility finished

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -141,14 +141,18 @@
               (kill-buffer buffer)))
           (message "Killed %i dired buffer(s)." count ))))
 
-(defun dired-toggle-dotfile-visibility ()
-  "Show/hide dot-files TODO: show"
+(defun dired-dotfiles-toggle ()
+  "Show/hide dot-files"
   (interactive)
   (when (equal major-mode 'dired-mode)
-    (setq dired-dotfiles-visible-p nil)
-    (message "h")
-    (dired-mark-files-regexp "\\\.")
-    (dired-do-kill-lines)))
+    (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+	(progn
+	  (set (make-local-variable 'dired-dotfiles-show-p) nil)
+	  (message "h")
+	  (dired-mark-files-regexp "\\\.")
+	  (dired-do-kill-lines))
+      (progn (revert-buffer) ; otherwise just revert to re-show
+	     (set (make-local-variable 'dired-dotfiles-show-p) t)))))
 
 ;; Auto-revert mode
 (add-hook 'dired-mode-hook 'turn-on-auto-revert-mode)
