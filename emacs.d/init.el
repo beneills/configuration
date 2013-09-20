5d4a703db393db76ea5a50e9fb2f15405bd84830
Ben Eills <ben@beneills.com>
2013-09-20 14:45:57 +0100

    + MELPA archive (for icicles)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,7 +12,13 @@
 ;; Initialize packages before other init stuff
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("marmalade" . "http://marmalade-repo.org/packages/")
+	     'APPEND)
+
+(add-to-list 'package-archives
+                     '("melpa" . "http://melpa.milkbox.net/packages/")
+                     'APPEND)
+
 (setq package-enable-at-startup nil)
 (package-initialize)
 
