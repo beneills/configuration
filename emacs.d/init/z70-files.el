f13364961a3dee4b1311c38cc300a707db8d8fa0
Ben Eills <ben@beneills.com>
2013-12-31 21:26:20 +0000

    Do not autoload files

--- emacs.d/init/z70-files.el
+++ emacs.d/init/z70-files.el
@@ -7,23 +7,24 @@
 		      ("bash-config" . "~/conf/bashrc")
 		      ("awesome-config" . "~/conf/config/awesome/rc.lua")
 		      ("x-config" . "~/conf/xsession")
-		      ("plan" . ,plan-path)))
+;		      ("plan" . ,plan-path)
+		      ))
 
 (setq default-directories '(("bin" . "~/bin")
 			    ("projects" . "~/projects"))) ; we want ~/projects last
 
 ;; TODO merge these?
 
-(dolist (item default-files)
-  (let ((name (car item))
-	(path (cdr item)))
-     (unless (get-file-buffer path)
-       (find-file path)
-       (rename-buffer name))))
+;; (dolist (item default-files)
+;;   (let ((name (car item))
+;; 	(path (cdr item)))
+;;      (unless (get-file-buffer path)
+;;        (find-file path)
+;;        (rename-buffer name))))
 
-(dolist (item default-directories)
-  (let ((name (car item))
-	(path (cdr item)))
-     (unless (get-buffer name)
-       (dired path)
-       (rename-buffer name))))
+;; (dolist (item default-directories)
+;;   (let ((name (car item))
+;; 	(path (cdr item)))
+;;      (unless (get-buffer name)
+;;        (dired path)
+;;        (rename-buffer name))))
