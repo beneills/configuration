12a23fb97263b213fbd3dde9d96786d2da9def74
Ben Eills <ben@beneills.com>
2015-12-05 01:17:45 +0000

    give more feedback when creating symlinks

--- conf_create_symlinks.py
+++ conf_create_symlinks.py
@@ -16,14 +16,20 @@ for f in entries:
     source_path = os.path.join(SOURCE_DIR, f)
     target_path = os.path.join(TARGET_DIR, '.' + f)
     if os.path.exists(target_path):
-        print "Skipping '{0}': symlink exists.".format(f)
-        continue
-    try:
-        print "Linking {1} -> {0}".format(source_path, target_path)
-        os.symlink(source_path, target_path)
-        count += 1
-    except OSError as e:
-        print "Cannot create sylink: {0}".format(e)
+        type = "file"
+        if os.path.islink(target_path):
+            type = "link"
+        elif os.path.isdir(target_path):
+            type = "directory"
+
+        print "Skipping '{0}': {1} exists.".format(f, type)
+    else:
+        try:
+            print "Linking {1} -> {0}".format(source_path, target_path)
+            os.symlink(source_path, target_path)
+            count += 1
+        except OSError as e:
+            print "Error creating sylink: {0}".format(e)
 
 
 print "Created {0} symlinks in {1}".format(count, TARGET_DIR)
